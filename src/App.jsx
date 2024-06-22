05ba65692c0eab02a375d20231568d724a9f7a7e
prathnow <prathnow@gmail.com>
2024-06-23 00:07:42 +0200

    Remove unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import GlobalStyles from "./styles/GlobalStyles";
 import MainPage from "./pages/MainPage";
-import Test from "./pages/Test";
 
 function App() {
   return (
