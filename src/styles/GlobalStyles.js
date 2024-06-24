aeba32c1045f7a25f5575b0e562dcd9d3b5cc4eb
prathnow <prathnow@gmail.com>
2024-06-24 16:15:24 +0200

    Add some style variables

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -39,6 +39,12 @@ const GlobalStyles = styled.createGlobalStyle`
     --border-radius-sm: 5px;
     --border-radius-md: 7px;
     --border-radius-lg: 9px;
+
+    --backdrop-color: rgba(0, 0, 0, 0.3);
+
+    --shadow-sm: 0 1px 2px rgba(0, 0, 0, 0.4);
+    --shadow-md: 0px 0.6rem 2.4rem rgba(0, 0, 0, 0.3);
+    --shadow-lg: 0 2.4rem 3.2rem rgba(0, 0, 0, 0.4);
   }
 
   *,
