03140a4a42225a6407fc372c57affd43e619cddb
prathnow <prathnow@gmail.com>
2024-07-28 17:21:31 +0200

    Install React Query

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import GlobalStyles from "./styles/GlobalStyles";
 import MainPage from "./pages/MainPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -17,7 +26,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
