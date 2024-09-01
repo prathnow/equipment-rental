244a5b724d92100ce404cf256960c4ff97902115
prathnow <prathnow@gmail.com>
2024-09-01 13:52:46 +0200

    Add sections to Main Page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,23 @@
+import AboutUsSection from "../components/AboutUsSection";
+import HomeSearchBar from "../components/HomeSearchBar";
+import NewArrivalsSection from "../components/NewArrivalsSection";
+import NewsSection from "../components/NewsSection";
+import OurServicesSection from "../components/OurServicesSection";
+import Row from "../ui/Row";
+
 function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <>
+      <Row $background="#f0be1b">
+        <HomeSearchBar />
+      </Row>
+
+      <AboutUsSection />
+      <NewArrivalsSection />
+      <OurServicesSection />
+      <NewsSection />
+    </>
+  );
 }
 
 export default MainPage;
