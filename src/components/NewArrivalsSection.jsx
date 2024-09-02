4e38cf95bd246d3c70d1a41ff4eb7d99693e1f21
prathnow <prathnow@gmail.com>
2024-09-02 18:21:32 +0200

    Remove static data

--- src/components/NewArrivalsSection.jsx
+++ src/components/NewArrivalsSection.jsx
@@ -15,53 +15,6 @@ function NewArrivalsSection() {
   const { isLoading, newEquipments } = useNewEquipments();
 
   if (isLoading) return <Spinner />;
-  console.log(newEquipments);
-
-  // Example data, replace with real data
-  const newArrivals = [
-    {
-      id: 1,
-      title: "PC210LC-11",
-      description: "PC210LC-11 KOPARKA KOMATSU GĄSIENICOWA STAGEIV",
-      imageUrl:
-        "https://ramionline.ramirent.pl/img/eq/PL/_MODELS/I005034.jpg?ver=1689234691",
-    },
-    {
-      id: 2,
-      title: "D601APG AUSA WOZIDŁO",
-      description: "WOZIDŁA BUDOWLANE O ŁADOWNOŚCI DO 7000 KG",
-      imageUrl:
-        "https://ramionline.ramirent.pl/img/eq/PL/WZBUD.jpg?ver=1689322301",
-    },
-    {
-      id: 3,
-      title: "PW160 KOMATSU",
-      description: "KOPARKA KOŁOWA O MASIE 14-16T",
-      imageUrl:
-        "https://ramionline.ramirent.pl/img/eq/PL/KOKOB.jpg?ver=1654001187",
-    },
-    {
-      id: 4,
-      title: "PC210LC-11",
-      description: "PC210LC-11 KOPARKA KOMATSU GĄSIENICOWA STAGEIV",
-      imageUrl:
-        "https://ramionline.ramirent.pl/img/eq/PL/_MODELS/I005034.jpg?ver=1689234691",
-    },
-    {
-      id: 5,
-      title: "D601APG AUSA WOZIDŁO",
-      description: "WOZIDŁA BUDOWLANE O ŁADOWNOŚCI DO 7000 KG",
-      imageUrl:
-        "https://ramionline.ramirent.pl/img/eq/PL/WZBUD.jpg?ver=1689322301",
-    },
-    {
-      id: 6,
-      title: "PW160 KOMATSU",
-      description: "KOPARKA KOŁOWA O MASIE 14-16T",
-      imageUrl:
-        "https://ramionline.ramirent.pl/img/eq/PL/KOKOB.jpg?ver=1654001187",
-    },
-  ];
 
   return (
     <Section $background="--main-background-color">
